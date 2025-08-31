faca4e3fa95e87240aff73fbeac1ca2acd04dda7
Renkai33s <113019334+Renkai33s@users.noreply.github.com>
2025-09-01 01:29:58 +0300

    Update ShareLogs.js

--- ShareLogs.js
+++ ShareLogs.js
@@ -1,8 +1,21 @@
 javascript:(function(){
-    var url = window.location.href;
-    var newUrl = url.replace(/,savedSearch:'[^']*'/, '');
-    if(newUrl !== url){
-        history.replaceState(null, '', newUrl);
-        window.location.reload();
+    // Убираем savedSearch из hash
+    var hash = window.location.hash;
+    var newHash = hash.replace(/,savedSearch:'[^']*'/, '');
+    if(newHash !== hash){
+        history.replaceState(null, '', window.location.pathname + window.location.search + newHash);
     }
+
+    // Ждем появления кнопки Share
+    function clickShareButton() {
+        var btn = document.querySelector('[data-test-subj="shareTopNavButton"]') || 
+                  document.querySelector('button[aria-label="Share"]');
+        if(btn){
+            btn.click();
+            console.log("Share button clicked!");
+            clearInterval(interval);
+        }
+    }
+
+    var interval = setInterval(clickShareButton, 500);
 })();
